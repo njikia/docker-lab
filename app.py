63f9844b61699d82eef3e8713b968c25c9be7b16
Nikita <jikianika@gmail.com>
2026-09-03 20:57:43 +0400

    Fixing critical application bug

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(500)
+        self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello from Kubernetes!")
 
